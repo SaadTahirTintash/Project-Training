64b5e5172c40fa6e21dbcb9cbf53fe897f3eb063
SaadTahirTintash <saad.tahir@tintash.com>
2019-06-27 15:53:59 +0500

    Initial Commit

---
+++ Footy-Crazy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Footy-Crazy
+//
+//  Created by Tintash on 27/06/2019.
+//  Copyright © 2019 Tintash. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
