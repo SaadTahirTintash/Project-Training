60df148b7eeb307cfac0b6d4f59213386e404180
SaadTahirTintash <saad.tahir@tintash.com>
2019-07-22 13:20:24 +0500

    Indentation

--- Footy-Crazy/DataManager/FCDataManager.swift
+++ Footy-Crazy/DataManager/FCDataManager.swift
@@ -6,14 +6,13 @@
 //  Copyright © 2019 Tintash. All rights reserved.
 //
 
-class FCDataManager {
-    
-    static let shared = FCDataManager()
-    var newsFeedData      : [FCNewsFeedModel]         = []
-    var galleryData       : [FCGalleryModel]          = []
-    var teamsData         : [FCTeamsModel]            = []
-    var playersData       : [FCPlayersModel]          = []
-    var citiesLocationData: [FCCitiesLocationModel]   = []
+class FCDataManager {    
+    static let shared       : FCDataManager             = FCDataManager()
+    var newsFeedData        : [FCNewsFeedModel]         = []
+    var galleryData         : [FCGalleryModel]          = []
+    var teamsData           : [FCTeamsModel]            = []
+    var playersData         : [FCPlayersModel]          = []
+    var citiesLocationData  : [FCCitiesLocationModel]   = []
     private init(){}
 }
 
